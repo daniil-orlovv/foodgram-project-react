ee01dab30049b429c1d06935968842af869f46e8
Даниил Орлов <danilorlov9898@yandex.ru>
2023-10-05 17:34:45 +0300

    Добавил разрешения для RecipeViewSet

--- foodgram/api/permissions.py
+++ foodgram/api/permissions.py
@@ -1,6 +1,17 @@
 from rest_framework import permissions
 
 
-class OnlyAuthorized(permissions.BasePermission):
+class UpdateIfAuthor(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in ("PATCH", "DELETE"):
+            return obj.author == request.user
+        else:
+            return True
+
+
+class CreateIfAuth(permissions.BasePermission):
     def has_permission(self, request, view):
-        return (request.user.is_authenticated)
+        if request.method == "POST":
+            return request.user.is_authenticated
+        else:
+            return True
