4d9441137d20ff3c945eae968d7fd2b10bf754ee
Даниил Орлов <danilorlov9898@yandex.ru>
2023-10-11 23:04:28 +0300

    Добавил фильтр по тегам через связанные поля

---
+++ foodgram/api/filters.py
@@ -0,0 +1,15 @@
+import django_filters
+
+from recipes.models import Recipe, Tag
+
+
+class TagFilter(django_filters.FilterSet):
+    tags = django_filters.ModelMultipleChoiceFilter(
+        field_name='tags__slug',
+        to_field_name='slug',
+        queryset=Tag.objects.all(),
+    )
+
+    class Meta:
+        model = Recipe
+        fields = ['tags']
