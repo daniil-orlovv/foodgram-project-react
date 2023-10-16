9e418667d50ebd0bc3b089ca53b0820605b1ff7d
Даниил Орлов <danilorlov9898@yandex.ru>
2023-10-17 02:43:35 +0300

    Добавил 2 новых фильтра - is_in_shopping_cart и author

--- foodgram/api/filters.py
+++ foodgram/api/filters.py
@@ -12,6 +12,12 @@ class RecipeFilter(django_filters.FilterSet):
     is_favorited = django_filters.NumberFilter(
         method='filter_is_favorited'
     )
+    is_in_shopping_cart = django_filters.NumberFilter(
+        method='filter_is_in_shopping_cart'
+    )
+    author = django_filters.NumberFilter(
+        method='filter_author'
+    )
 
     class Meta:
         model = Recipe
@@ -21,5 +27,19 @@ class RecipeFilter(django_filters.FilterSet):
         user = self.request.user
         if value and user.is_authenticated:
             return queryset.filter(favorite_recipe__user=user)
+        else:
+            return queryset
+
+    def filter_is_in_shopping_cart(self, queryset, name, value):
+        user = self.request.user
+        if value and user.is_authenticated:
+            return queryset.filter(shop_item__user=user)
+        else:
+            return queryset
+
+    def filter_author(self, queryset, name, value):
+        user = self.request.user
+        if value and user.is_authenticated:
+            return queryset.filter(author=value)
         else:
             return queryset
