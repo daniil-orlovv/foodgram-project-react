fb008cabb0a60ec3319f45afc262e611eeb2d70a
Даниил Орлов <danilorlov9898@yandex.ru>
2023-10-17 22:50:24 +0300

    Изменил urls

--- foodgram/api/urls.py
+++ foodgram/api/urls.py
@@ -7,25 +7,25 @@ from .views import (RecipeViewSet, ShopViewSet, FavoriteViewSet,
 
 router = SimpleRouter()
 
-
-router.register(r'users', CustomUserViewSet, basename='get_user')
-router.register(
-    'users/subscriptions', FollowViewSet, basename='getfollows')
-router.register(
-    r'users/(?P<id>\d+)/subscribe', FollowViewSet, basename='follow')
+router.register('users', CustomUserViewSet, basename='users')
+router.register('tags', TagViewSet, basename='tags')
 router.register('recipes', RecipeViewSet, basename='recipes')
+router.register('cart', ShopViewSet, basename='get_shop_list')
 router.register(r'recipes/(?P<id>\d+)/shopping_cart', ShopViewSet,
                 basename='shop')
 router.register(r'recipes/(?P<id>\d+)/favorite', FavoriteViewSet,
                 basename='favorite')
-router.register('tags', TagViewSet, basename='tags')
-router.register('cart', ShopViewSet, basename='get_shop_list')
+router.register(r'users/(?P<id>\d+)/subscribe', FollowViewSet,
+                basename='follow')
+router.register(r'users/(?P<id>\d+)', CustomUserViewSet,
+                basename='get_user')
 router.register('ingredients', IngredientViewSet, basename='ingredients')
 
 urlpatterns = [
     path('recipes/download_shopping_cart/',
          ShopViewSet.as_view({'get': 'download'})),
     path('users/me/', CustomUserViewSet.as_view({'get': 'me'})),
+    path('users/subscriptions/', FollowViewSet.as_view({'get': 'subscriptions'})),
     path('', include(router.urls)),
     path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
