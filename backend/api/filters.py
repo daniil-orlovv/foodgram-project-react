e6a9e0ae19d7ea5117b057d8b2796a7a5127b921
Даниил Орлов <danilorlov9898@yandex.ru>
2023-10-23 19:15:59 +0300

    Изменил указание related_name в def filter_is_favorited

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -26,7 +26,7 @@ class RecipeFilter(django_filters.FilterSet):
     def filter_is_favorited(self, queryset, name, value):
         user = self.request.user
         if value and user.is_authenticated:
-            return queryset.filter(favorite_recipe__user=user)
+            return queryset.filter(recipe_favorites__user=user)
         return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
