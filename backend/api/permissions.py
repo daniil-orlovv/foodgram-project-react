9f49bdd93d55dd8e844aa7084d1fa2dd34edbd9d
Даниил Орлов <danilorlov9898@yandex.ru>
2023-10-23 16:53:09 +0300

    Убрал лишний else из методов пермишнов

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -5,13 +5,11 @@ class UpdateIfAuthor(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in ("PATCH", "DELETE"):
             return obj.author == request.user
-        else:
-            return True
+        return True
 
 
 class CreateIfAuth(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method == "POST":
             return request.user.is_authenticated
-        else:
-            return True
+        return True
