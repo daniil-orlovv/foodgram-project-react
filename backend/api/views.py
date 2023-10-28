41444e7813cb60366e83ae53a471456a6b6d030b
Даниил Орлов <danilorlov9898@yandex.ru>
2023-10-29 00:00:43 +0300

    Поправил методы создания и удаления подписки

--- backend/api/views.py
+++ backend/api/views.py
@@ -72,8 +72,7 @@ class FollowViewSet(viewsets.ModelViewSet):
             })
         return paginator.get_paginated_response(serializer.data)
 
-    @action(detail=False, methods=['post'])
-    def subscribe(self, request, *args, **kwargs):
+    def create(self, request, *args, **kwargs):
         user = request.user
         author_id = kwargs.get('id')
         author = get_object_or_404(CustomUser, id=author_id)
@@ -88,7 +87,7 @@ class FollowViewSet(viewsets.ModelViewSet):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     @action(detail=True, methods=['delete'])
-    def unsubscribe(self, request, *args, **kwargs):
+    def delete(self, request, *args, **kwargs):
         author_id = kwargs.get('id')
         if not Follow.objects.filter(author=author_id).exists():
             return Response({
