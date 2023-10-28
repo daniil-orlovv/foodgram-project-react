b7c475325978264a60a9c273274081ca4b7db850
Даниил Орлов <danilorlov9898@yandex.ru>
2023-10-28 23:39:14 +0300

    Переместил подписки в urlpatterns

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -15,8 +15,8 @@ router.register(r'recipes/(?P<id>\d+)/shopping_cart', ShopViewSet,
                 basename='shop')
 router.register(r'recipes/(?P<id>\d+)/favorite', FavoriteViewSet,
                 basename='favorite')
-router.register(r'users/(?P<id>\d+)/subscribe', FollowViewSet,
-                basename='follow')
+# router.register(r'users/(?P<id>\d+)/subscribe', FollowViewSet,
+#                 basename='follow')
 router.register('ingredients', IngredientViewSet, basename='ingredients')
 
 urlpatterns = [
@@ -27,6 +27,10 @@ urlpatterns = [
          ShopViewSet.as_view({'get': 'download'})),
     path('users/subscriptions/', FollowViewSet.as_view(
         {'get': 'subscriptions'})),
+    path(r'users/(?P<id>\d+)/subscribe/', FollowViewSet.as_view({
+        'post': 'subscribe',
+        'delete': 'unsubscribe',
+    }))
 
 
 ]
