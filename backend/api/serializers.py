75fa5663d0ef15e64a4f8bbf1d938236d7469630
Даниил Орлов <danilorlov9898@yandex.ru>
2023-10-28 16:46:19 +0300

    Поправил pep8

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -315,7 +315,8 @@ class FollowSerializer(serializers.ModelSerializer):
             user = request.user
             limit = request.GET.get('recipes_limit')
             recipes = user.author_recipes.filter(author=obj)[:int(limit)]
-            return FavoriteCartSerializer(recipes, many=True, read_only=True).data
+            return FavoriteCartSerializer(
+                recipes, many=True, read_only=True).data
 
     def get_recipes_count(self, obj):
         request = self.context.get('request')
