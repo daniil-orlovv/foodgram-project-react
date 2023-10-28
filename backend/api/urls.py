b525e8997e0e7019163b26c9c3a3580316806ca9
Даниил Орлов <danilorlov9898@yandex.ru>
2023-10-28 15:21:20 +0300

    Удалил все урлы с users, оставил только подключение джосера

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,13 +1,11 @@
 from django.urls import include, path
-from djoser.views import UserViewSet as DjoserUserViewSet
 from rest_framework.routers import SimpleRouter
 
-from .views import (CustomUserViewSet, FavoriteViewSet, FollowViewSet,
-                    IngredientViewSet, RecipeViewSet, ShopViewSet, TagViewSet)
+from .views import (FavoriteViewSet, FollowViewSet, IngredientViewSet,
+                    RecipeViewSet, ShopViewSet, TagViewSet)
 
 router = SimpleRouter()
 
-router.register('users', DjoserUserViewSet, basename='users')
 router.register('tags', TagViewSet, basename='tags')
 router.register('recipes', RecipeViewSet, basename='recipes')
 router.register('cart', ShopViewSet, basename='get_shop_list')
@@ -17,8 +15,6 @@ router.register(r'recipes/(?P<id>\d+)/favorite', FavoriteViewSet,
                 basename='favorite')
 router.register(r'users/(?P<id>\d+)/subscribe', FollowViewSet,
                 basename='follow')
-router.register(r'users/(?P<id>\d+)', CustomUserViewSet,
-                basename='get_user')
 router.register('ingredients', IngredientViewSet, basename='ingredients')
 
 urlpatterns = [
