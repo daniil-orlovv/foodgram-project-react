c24fcecced8449e87d220a40dbe947d8485d37b2
Даниил Орлов <danilorlov9898@yandex.ru>
2023-10-29 01:24:15 +0300

    Попытка поправить урлы

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -15,16 +15,17 @@ router.register(r'recipes/(?P<id>\d+)/shopping_cart', ShopViewSet,
                 basename='shop')
 router.register(r'recipes/(?P<id>\d+)/favorite', FavoriteViewSet,
                 basename='favorite')
-router.register(r'users/(?P<id>\d+)/subscribe', FollowViewSet,
-                basename='follow')
+# 'router.register(r'users/(?P<id>\d+)/subscribe', FollowViewSet,
+#                 basename='follow')'
 router.register('ingredients', IngredientViewSet, basename='ingredients')
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('recipes/download_shopping_cart/',
          ShopViewSet.as_view({'get': 'download'})),
+    path(r'users/(?P<id>\d+)/subscribe', FollowViewSet.as_view()),
     path('users/subscriptions/', FollowViewSet.as_view(
         {'get': 'subscriptions'})),
-    path('', include(router.urls)),
     path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 ]
