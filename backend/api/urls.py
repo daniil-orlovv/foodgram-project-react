71d7ac89cb8e53cc16cb4e0806915819f218b4e4
Даниил Орлов <danilorlov9898@yandex.ru>
2023-10-29 19:29:51 +0300

    поменял местами урлы

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -11,21 +11,17 @@ router.register('users', DjoserUserViewSet, basename='users')
 router.register('tags', TagViewSet, basename='tags')
 router.register('recipes', RecipeViewSet, basename='recipes')
 router.register('cart', ShopViewSet, basename='get_shop_list')
-router.register(r'recipes/(?P<id>\d+)/shopping_cart', ShopViewSet,
-                basename='shop')
-router.register(r'recipes/(?P<id>\d+)/favorite', FavoriteViewSet,
-                basename='favorite')
-router.register(r'users/(?P<id>\d+)/subscribe', FollowViewSet,
-                basename='follow')
 router.register('ingredients', IngredientViewSet, basename='ingredients')
 
 urlpatterns = [
-    path('', include('djoser.urls')),
-    path('', include(router.urls)),
+    path(r'recipes/(?P<id>\d+)/shopping_cart', ShopViewSet.as_view()),
+    path(r'recipes/(?P<id>\d+)/favorite', FavoriteViewSet.as_view()),
+    path(r'users/(?P<id>\d+)/subscribe', FollowViewSet.as_view()),
     path('recipes/download_shopping_cart/',
          ShopViewSet.as_view({'get': 'download'})),
     path('users/subscriptions/', FollowViewSet.as_view(
         {'get': 'subscriptions'})),
-
+    path('', include('djoser.urls')),
+    path('', include(router.urls)),
     path('auth/', include('djoser.urls.authtoken')),
 ]
