3a8713006a5bc7ec6e95b7541a2c0bfaeab259ff
Даниил Орлов <danilorlov9898@yandex.ru>
2023-10-29 17:52:53 +0300

    поменял местами урлы

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -20,11 +20,12 @@ router.register(r'users/(?P<id>\d+)/subscribe', FollowViewSet,
 router.register('ingredients', IngredientViewSet, basename='ingredients')
 
 urlpatterns = [
+    path('', include('djoser.urls')),
     path('', include(router.urls)),
     path('recipes/download_shopping_cart/',
          ShopViewSet.as_view({'get': 'download'})),
     path('users/subscriptions/', FollowViewSet.as_view(
         {'get': 'subscriptions'})),
-    path('', include('djoser.urls')),
+
     path('auth/', include('djoser.urls.authtoken')),
 ]
