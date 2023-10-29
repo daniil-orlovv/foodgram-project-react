67dc1fbba347f3efa0be8772b2e6b20880949ab8
Даниил Орлов <danilorlov9898@yandex.ru>
2023-10-29 17:08:14 +0300

    добавил load_dotend()

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -9,6 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
+load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', default='secret_key')
 
